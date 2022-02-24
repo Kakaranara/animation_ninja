6af10d77468615375d5f4d60cd998f959c5f80d5
kakaranara <74239131+Kakaranara@users.noreply.github.com>
2022-02-24 17:13:15 +0700

    liked animation

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -5,16 +5,51 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with TickerProviderStateMixin {
+
+  bool isFav = false;
+  AnimationController _controller;
+  Animation _colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 200),
+    );
+
+    _colorAnimation = ColorTween(begin: Colors.grey, end: Colors.red).animate(_controller);
+
+    _controller.addStatusListener((status) {
+      if(status == AnimationStatus.completed){
+        isFav = true;
+      }
+      if(status == AnimationStatus.dismissed){
+        isFav = false;
+      }
+    });
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
-      ),
-      onPressed: () {},
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (BuildContext context, _){
+        return IconButton(
+          icon: Icon(
+            Icons.favorite,
+            color: _colorAnimation.value,
+            size: 30,
+          ),
+          onPressed: () {
+            isFav ? _controller.reverse() : _controller.forward();
+          },
+        );
+      },
     );
   }
 }
